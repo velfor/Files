c035a9849013ac86dfd460f0e706622f8ffb2742
группы стационара <st@itstep.lan>
2020-11-24 17:25:22 +0300

    Read from TEXT file

--- Source.cpp
+++ Source.cpp
@@ -5,11 +5,20 @@
 #include <string>
 using namespace std;
 int main() {
-	ofstream outFile;
-	outFile.open("my_file.txt");
-	string str;
-	getline(cin, str);
-	outFile << str;
-	outFile.close();
+	////запись в файл
+	//ofstream outFile;
+	//outFile.open("my_file.txt");
+	//string str;
+	//getline(cin, str);
+	//outFile << str;
+	//outFile.close();
+
+	//чтение из файла
+	ifstream inFile;
+	inFile.open("my_file.txt");
+	string str1;
+	getline(inFile,str1);
+	cout << str1;
+	inFile.close();
 	return 0;
 }
